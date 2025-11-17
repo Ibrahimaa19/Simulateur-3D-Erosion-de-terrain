6c0fe75ea1691aa19c1dd0bde45bae19971ca991
Sy Aboubacar <aboubacar.sy01.sy@gmail.com>
2025-11-17 21:01:33 +0100

    Ajout de la méthode pour charger les sommets du terrains.

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -1,4 +1,24 @@
 #include "terrain.hpp"
 
 
+void Terrain::load_vectices(){
+    vertices.clear();
+    for (int z = 0; z < height; z++) {
+        for (int x = 0; x < width; x++) {
+            bool isBorder = (x < borderSize || x >= width - borderSize || z < borderSize || z >= height - borderSize);
+            int index = z * width + x;
+            float y = data[index] * 10.0f;
+            
+            
+            vertices.push_back((float)x);  
+            if(isBorder){
+                vertices.push_back(0.0f); 
+            }else{
+                vertices.push_back(y);
+            }
+                        
+            vertices.push_back((float)z);        
+        }
+    }
+}
 
--- src/terrain.hpp
+++ src/terrain.hpp
@@ -9,6 +9,12 @@ struct Terrain{
 	std::vector<float> vertices;
 	std::vector<unsigned int> indices;
 
+    /**
+     * @brief Contructeur de la structure de données Terrain avec l'image passer en paramètre
+     *
+     * @param image_path Un chemin vers une image à lire
+     * @return Une structure Terrain correspondant avec l'image passer en paramètre
+     */
     Terrain (const char* image_path){
         int t_channels;
         unsigned char* image = stbi_load(image_path, &this->width, &this->height, &t_channels, 1);
@@ -23,4 +29,16 @@ struct Terrain{
 
         stbi_image_free(image);
     }
+
+    /**
+     * @brief Met à jours le vecteur vertices, en fonction des valeurs dans data
+     * 
+     * Pour chaque point du terrain, on va générer un triplet x,y,z où x et z représenterons une position sur un plan 2D et y l'élévation sur ce point(x,z).
+     * 
+     * Lorsque nous serons sur les bords, nous définirons la hauteur/élévation à 0. 
+     * C'est à dire, si x est compris en [0; borderSize] ou [width-borderSize;width] ou si, z est compris entre [0; borderSize] ou [height-borderSize;height]
+     */
+    void load_vectices();
+    
+    void load_incides();
 };
